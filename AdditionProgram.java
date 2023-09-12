d34fe905a9b466400015a8039657c4ae1e5b7d14
MrunalMate20 <144772752+MrunalMate20@users.noreply.github.com>
2023-09-12 16:04:20 +0530

    Add files via upload
    java code

---
+++ AdditionProgram.java
@@ -0,0 +1,17 @@
+public class AdditionProgram
+{ 
+public static void main(String args[])
+{
+System.out.println("My First Program in Java");
+int a=81,b=50;
+int add,sub,mul,div;
+add= a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+System.out.println("Addition :"+ add);
+System.out.println("Subtraction :"+ sub);
+System.out.println("Multiplication :" +mul);
+System.out.println("Division :"+ div);
+}
+}
---
+++ FirstProgram2.java
@@ -0,0 +1,11 @@
+public class FirstProgram2
+{ 
+public static void main(String args[])
+{
+System.out.println("My First Program in Java");
+int a=88,b=90;
+int c;
+c = a+b;
+System.out.println(c);
+}
+}
